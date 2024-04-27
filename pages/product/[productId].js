1a08b88296e68003f5b5b8647691d25ac328be47
pimlapas2468 <pimlapas2468@gmail.com>
2024-04-28 00:45:50 +0700

    comment on product page

--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -2,9 +2,30 @@ import { useState } from 'react';
 // import fs from 'fs';
 import productsData from '../product/product_list';
 import Header from '../components/Header';
+import CommentContainer from '../components/CommentContainer';
+import Container from '../components/Container';
 
 export default function ProductDetailPage({ product }) {
     const [quantities, setQuantities] = useState({});
+
+    const [comments, setComments] = useState([
+        {
+            name: "Red",
+            description: "โต๊ะสวยมากกกกกกก"
+        },
+        {
+            name: "Black",
+            description: "โต๊ะสวยมากกกกกกก"
+        },
+        {
+            name: "White",
+            description: "โต๊ะสวยมากกกกกกก"
+        },
+    ])
+
+    const handleComment = (newComment) => {
+        setComments([...comments, newComment]);
+    };
 
     const maxQuantity = product.quantity;
 
@@ -30,28 +51,36 @@ export default function ProductDetailPage({ product }) {
         <main>
             <Header />
             <div className="nvg" style={{ marginTop: '70px' }}>Web Application Name {'>'} Category {'>'} Product</div>
-            <div className='detail-page'>
-                <div className='product-image-detail-page'>
-                    <img src={product.image} alt={product.name} />
-                </div>
-                <div className='description'>
-                    <h1>{product.name}</h1>
-                    <p>{product.description}</p>
-                    <p>Price: ${product.price}</p>
-                    <p>Quantity: {product.quantity}</p>
-                    <div className="quantity-controls">
-                        <button onClick={() => decreaseQuantity(product.id)}>-</button>
-                        <span>{quantities[product.id] || 0}</span>
-                        <button onClick={() => increaseQuantity(product.id)}>+</button>
-                    </div>
-                    <div className="cart-buy-button">
-                        <button className='add-to-cart'>Add to cart</button>
-                        <span></span>
-                        <a href="#" className="buy">Buy</a>
 
+            <Container>
+                <div className='detail-page'>
+                    <div className='product-image-detail-page'>
+                        <img src={product.image} alt={product.name} />
+                    </div>
+                    <div className='description'>
+                        <h1>{product.name}</h1>
+                        <p>{product.description}</p>
+                        <p>Price: ${product.price}</p>
+                        <p>Quantity: {product.quantity}</p>
+                        <div className="quantity-controls">
+                            <button onClick={() => decreaseQuantity(product.id)}>-</button>
+                            <span>{quantities[product.id] || 0}</span>
+                            <button onClick={() => increaseQuantity(product.id)}>+</button>
+                        </div>
+                        <div className="cart-buy-button">
+                            <button className='add-to-cart'>Add to cart</button>
+                            <span></span>
+                            <a href="#" className="buy">Buy</a>
+
+                        </div>
                     </div>
                 </div>
-        </div>
+
+                <CommentContainer 
+                    items={comments}
+                    handleComment={handleComment}
+                />
+            </Container>
 
         </main>
         
